fc2d19e1019465d6e95bf526a80dde607fd3ac0e
chuno1030 <chuno1030@naver.com>
2021-08-23 21:05:43 +0900

    프로젝트 추가 및 기본 설정

--- VMSViewer/App.xaml.cs
+++ VMSViewer/App.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace VMSViewer
@@ -13,5 +8,10 @@ namespace VMSViewer
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
--- VMSViewer/MainWindow.xaml.cs
+++ VMSViewer/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VMSViewer
 {
@@ -23,6 +11,26 @@ namespace VMSViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitProc();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DoFinal();
+        }
+
+        private void InitProc()
+        {
+
+        }
+
+        private void DoFinal()
+        {
+
         }
     }
 }
